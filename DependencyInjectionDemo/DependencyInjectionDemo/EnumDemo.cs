a931b6a9541811fe0ca653a80c69fc3b2754409a
aannaanntt <69764784+aannaanntt@users.noreply.github.com>
2024-07-26 12:41:50 +0530

    Add files via upload

---
+++ DependencyInjectionDemo/DependencyInjectionDemo/EnumDemo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionDemo
+{
+    enum Days
+    {
+        Mon,
+        Tue,
+        wed
+    }
+    internal class EnumDemo
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello");
+            Days[] df = (Days[])Enum.GetValues(typeof(Days));
+            foreach (Days d in df)
+            {
+
+                Console.WriteLine(d);
+            }
+
+        }
+    }
+}
---
+++ DependencyInjectionDemo/DependencyInjectionDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿//namespace DependencyInjection
+//{
+//   public interface IAccount
+//    {
+//        void PrintDetails();
+//    }
+    
+//    class CA : IAccount
+//    {
+//        void IAccount.PrintDetails()
+//        {
+//            Console.WriteLine("Details of Current Account");
+//        }
+//    }
+//    class SA : IAccount
+//    {
+//        void IAccount.PrintDetails()
+//        {
+//            Console.WriteLine("Details from Saving Account");
+//        }
+//    }
+
+//    //class Account
+//    //{
+//    //    private IAccount account;
+//    //    public Account(IAccount account)
+//    //    {
+//    //        this.account = account;
+//    //    }
+
+//    //    public void PrintAccounts() { 
+//    //        account.PrintDetails();
+//    //    }
+            
+        
+//    //}
+
+//    class Account
+//    {
+//        public IAccount account { get; set; }
+
+//        public void PrintDetails() { 
+//            account.PrintDetails();
+//        }
+//    }
+    
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+      
+//            Account sa = new Account();
+//            sa.account= new SA();
+//            sa.PrintDetails();
+            
+//        }
+//    }
+
+//}
